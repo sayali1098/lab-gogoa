252121316be0e1d713f65a9371391c46a3c5029f
sayali1098 <sauamritkar@gmail.com>
2020-07-02 00:14:40 +0530

    FSD0620B102

--- src/services/Booking.java
+++ src/services/Booking.java
@@ -1,4 +1,10 @@
 package services;
 
 // Type your code
-
+public class Booking{
+	
+	public double booking(int noOfPersons,int rates) {
+		double totalRate = noOfPersons*rates;
+		return totalRate;
+	}
+}
--- src/services/FareCalculator.java
+++ src/services/FareCalculator.java
@@ -1,3 +1,50 @@
 package services;
 
+//import java.time.LocalDate;
+
+import model.Bus;
+import model.Flight;
+import model.Hotel;
+import model.Train;
+
 // Type your code
+public class FareCalculator extends Booking{
+	
+	public double book(Hotel hotel) {
+		int hotelRate = hotel.getRates();
+		int hotelPersons = hotel.getNoOfPersons();
+		double HotelBook = booking(hotelRate, hotelPersons);
+		
+//		LocalDate start=LocalDate.parse(hotel.getFromdate());		
+//		LocalDate end=LocalDate.parse(to);
+		
+		int Checkin = hotel.getFromdate().getDayOfMonth();
+		int Checkout = hotel.getTodate().getDayOfMonth();
+		if((Checkin < Checkout) );
+		return HotelBook;
+	}
+	
+	public double book(Flight flight) {
+		int flightRate = flight.getRates();
+		int flightPersons = flight.getNoOfPersons();
+		double FlightBook = booking(flightRate, flightPersons);
+		String trip = flight.getTriptype();
+		if(trip == "one-way");
+		if(trip == "round-trip");
+		return FlightBook;
+	}
+	
+	public double book(Train train) {
+		int trainRate = train.getRates();
+		int trainPersons = train.getNoOfPersons();
+		double trainBook = booking(trainRate, trainPersons);
+		return trainBook;
+	}
+	
+	public double book(Bus bus) {
+		int busRate = bus.getRates();
+		int busPersons = bus.getNoOfPersons();
+		double busBook = booking(busRate, busPersons);
+		return busBook;
+	}
+}
